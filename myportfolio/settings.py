e4d689766e7b363572f0f5ae3ae51168dab9bfc7
7camc7 <claudiachurch00@gmail.com>
2022-07-16 12:36:24 -0600

    local

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -138,3 +138,8 @@ MEDIA_URL = '/media/' #where soemone accesses images
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from . local_settings import *
+except ImportError:
+    pass
